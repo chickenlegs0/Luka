a9044b1f54111179d7c76acb206ce985b1e4920b
Schubird <daramosmusic@gmail.com>
2018-07-26 09:47:33 -0700

    Major Sound Update
    AAAA

---
+++ Assets/Luka/Extra Scripts/ObjectInfo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Stores info about the transform of an object, as well as if its been destroyed
+///
+/// Ruben Sanchez
+/// 7/19/18
+/// </summary>
+[System.Serializable]
+public class ObjectInfo 
+{
+    public Vector2 savedPosition;
+    public Vector3 savedEulerAngles;
+    public bool hasBeenDestroyed;
+
+    public ObjectInfo(Vector2 newPosition, Vector3 newEulerAngles, bool newHasBeenDestroyed)
+    {
+        savedPosition = newPosition;
+        savedEulerAngles = newEulerAngles;
+        hasBeenDestroyed = newHasBeenDestroyed;
+    }
+}
---
+++ Assets/Luka/Extra Scripts/SaveObjectState.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Serializes ObjectInfo
+///
+/// Ruben Sanchez
+/// 7/19/18
+/// </summary>
+public class SaveObjectState : MonoBehaviour
+{
+    void OnDisable()
+    {
+        SaveState(false);
+    }
+
+    void OnEnable()
+    { 
+        LoadState();
+    }
+
+    public void Reset(ObjectInfo info)
+    {
+        if (info.hasBeenDestroyed)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        transform.position = info.savedPosition;
+        transform.eulerAngles = info.savedEulerAngles;
+    }
+
+    public void SaveState(bool destroyed)
+    {
+        ObjectInfo newInfo = new ObjectInfo(transform.position, transform.eulerAngles, destroyed);
+
+        string serialized = JsonUtility.ToJson(newInfo); 
+        PlayerPrefs.SetString(SceneManager.GetActiveScene().name + gameObject.name, serialized);
+    }
+
+    public void LoadState()
+    {
+        string serialized = PlayerPrefs.GetString(SceneManager.GetActiveScene().name + gameObject.name);
+
+        if (!string.IsNullOrEmpty(serialized))
+        {
+            Reset(JsonUtility.FromJson<ObjectInfo>(serialized));
+        }
+    }
+}
