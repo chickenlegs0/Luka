ba5a4bfa7891758d7ed7d6ecd572cdbecd2557c7
Schubird <daramosmusic@gmail.com>
2018-07-27 11:32:47 -0700

    Elevator is broken jeezz
    AAA

---
+++ Assets/OnCollisionEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnCollisionEvent : MonoBehaviour {
+
+	public string tag = "Player";
+
+	public UnityEvent OnCollision;
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if(coll.gameObject.CompareTag(tag))
+			{
+				OnCollision.Invoke();
+			}
+	}
+				
+}
